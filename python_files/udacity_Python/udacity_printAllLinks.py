6531283ca70165d7b946948931192d1d682ed4e9
pTricKg <ptrickg1976@gmail.com>
2012-11-15 19:29:15 -0700

    prctc

--- python_files/udacity_Python/udacity_printAllLinks.py
+++
@@ -1,8 +0,0 @@
-def print_all_links(page):
-    while start_link == 1:
-        url, endpos = get_next_target(page)
-        if url:
-            print(url)
-            page = page[endpos:]
-        else:
-            return None, 0
