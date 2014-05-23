031dc2c159584ad253a880181bfdf6db467788de
pTricKg <ptrickg1976@gmail.com>
2014-05-22 23:53:53 -0600

    some new prgms

---
+++ python_files/words_read.py
@@ -0,0 +1,16 @@
+# Read from file
+ 
+
+fname = input("Enter file name: ")
+fh = open(fname, 'r')
+rd = fh.read().strip()
+
+# upper case
+print (rd.upper())
+
+# lower case
+print (rd.lower())
+
+# seperate words into seperate strings
+print (rd.split())
+
