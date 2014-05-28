c5442d2d7b9f3aca9503893a655bb2add88a2f8b
pTricKg <ptrickg1976@gmail.com>
2014-05-27 22:52:13 -0600

    Missed this one

---
+++ python_files/playing_around.py
@@ -0,0 +1,12 @@
+one = "bewareoftheenigmagroupunderground"
+two = "betterofftohacktherealownersboxes"
+three = "wassaidbyadudepseudodpsychomarine"
+word = []
+
+for l in one:
+    word.append(l + " ")
+    
+    print(word)
+
+print(one[0:1])
+print(one)
