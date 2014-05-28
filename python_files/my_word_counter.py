e509f0d0f97dfca9792f89d9b0a9ccea1670b794
pTricKg <ptrickg1976@gmail.com>
2014-05-28 01:42:28 -0600

    Some new

--- python_files/my_word_counter.py
+++ python_files/my_word_counter.py
@@ -1,9 +1,34 @@
+import os
 
 def my_counter():
-    name = input('Enter file:')
-    handle = open(name, 'r')
-    text = handle.read()
-    words = text.split()
+    name = input('Enter file or path to file: ')
+    dr = os.getcwd()
+    print("You are here:",dr)
+    print(name, "is your input")
+##    file_open = os.chdir(name)
+##    print(file_open)
+##    file_open = os.getcwd()
+##    print(file_open)
+    #try:
+        
+    if name.startswith("/"):
+        handle = open(name, 'r')
+        text = handle.read()
+        words = text.split()            
+    else:
+        file_open = os.getcwd()
+        print("Your file isn't located in this directory:",file_open)
+        clarify = input("You must clarify where your file is.")
+        print(clarify)
+        chngdir = os.chdir(clarify)
+        print(chngdir)
+        file_open = os.getcwd()
+        handle = open(file_open.strip() + "/" + name, 'r')
+        text = handle.read()
+        words = text.split()
+##    except:
+##        print("Something isn't right.  Please start again")
+##        my_counter()
 
     count=0
     for word in words:
@@ -13,6 +38,9 @@ def my_counter():
 
     user_decides = input("Would you like to count more?:")
     if user_decides == str('yes' or 'Yes'):
+        my_counter()
+    elif user_decides != str('yes' or 'Yes'):
+        input("Yes to continue or anything else to not")
         my_counter()
     else:
         print ("Good Bye")
