14f5e6c29c2456a8cf2bfe9d8053444f14fdaa29
pTricKg <ptrickg1976@gmail.com>
2014-05-29 19:59:19 -0600

    Fixed bug

--- python_files/read_email_part_two.py
+++ python_files/read_email_part_two.py
@@ -1,17 +1,17 @@
 fname = input("Enter file name: ")
-#if len(fname) < 1 : fname = "mbox-short.txt"
+if len(fname) < 1 : fname = "mbox-short.txt"
 
 fh = open(fname, 'r')
 count = 0
 
 for line in fh:
-    if line.startswith("From"):
+    if line.startswith("From "): #added  space to be sure not to include From:
         splt = line.split()
 ##        spc = line.find(" ")
 ##        spc2 = line.find(" ", spc)
 ##        #print(line[spc:" "])
 ##        print(line[spc:])
-        print(splt)
+        #print(splt)
         print(splt[1]) # this is what we're looking for
         count = count + 1
         print("this is count:",count)
