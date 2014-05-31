dac1d5886d84948960d538db57ac220744f9b09b
pTricKg <ptrickg1976@gmail.com>
2014-05-30 22:26:08 -0600

    Changed to floats and used round to second decimal

--- python_files/wage_earn.py
+++ python_files/wage_earn.py
@@ -1,14 +1,14 @@
 # Calculate Earnings
 
 ##def wage():
-inp = int(input("What is your hourly rate: "))
-inp2 = int(input("How many hours are you working: "))
+inp = float(input("What is your hourly rate: "))
+inp2 = float(input("How many hours are you working: "))
 
 wkly = inp * inp2
-print("This is your earnings for hours given:",wkly)
+print("This is your earnings for hours given:",round(wkly,2))
 mnthly = (wkly * 52) / 12
-print("This is your monthly earnings:",mnthly)
+print("This is your monthly earnings:",round(mnthly,2))
 annum = wkly * 52
-print("This is your annual earnings:",annum)
+print("This is your annual earnings:",round(annum,2))
 
 #def wage()
