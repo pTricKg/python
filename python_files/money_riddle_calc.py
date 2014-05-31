38c0dedc0f305840401d997c36f2363f5863e5da
pTricKg <ptrickg1976@gmail.com>
2014-05-30 21:33:39 -0600

    Small edit

--- python_files/money_riddle_calc.py
+++ python_files/money_riddle_calc.py
@@ -3,9 +3,10 @@ month = 30
 cash = 0.01
 
 while day < month:
+    day = day + 1
     cash = cash * 2
     print("This is current day:",day)
     print("This is current value:",cash)
     for days in range(30):
         days = days * 2
-    print(days)
+    
