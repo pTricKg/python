5049a93ca142eae7dd02a3810185278bc05d72aa
pTricKg <ptrickg1976@gmail.com>
2014-06-02 15:49:56 -0600

    Added another loop for averaging given numbers

--- python_files/enter_int_till_done.py
+++ python_files/enter_int_till_done.py
@@ -25,3 +25,16 @@ for num in numbers :
 
 print 'Maximum:', maximum
 print 'Minimum:', minimum
+
+
+total = 0
+count = 0
+
+while True:
+	inp = input('Enter a number:')
+	if inp == 'done: break
+	value = float(inp)
+	total = total + value
+	count = count + 1
+average = total / count
+print('Average:',average)
