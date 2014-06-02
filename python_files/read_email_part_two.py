17b5de07b98d112169896a085bd3b587c7b88cb1
pTricKg <ptrickg1976@gmail.com>
2014-06-02 17:46:28 -0600

    Added find domain

--- python_files/read_email_part_two.py
+++ python_files/read_email_part_two.py
@@ -7,14 +7,13 @@ count = 0
 for line in fh:
     if line.startswith("From "): #added  space to be sure not to include From:
         splt = line.split()
-##        spc = line.find(" ")
-##        spc2 = line.find(" ", spc)
-##        #print(line[spc:" "])
-##        print(line[spc:])
-        #print(splt)
+
         print(splt[1]) # this is what we're looking for
         count = count + 1
         print("this is count:",count)
+        slt2 = splt[1].split('@')
+        domain = slt2[1]
+        print("this is domain:",domain)
     else:
         continue
     
