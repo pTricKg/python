4a9d598195daebc0dadcafbf348f176cefcaa7db
pTricKg <ptrickg1976@gmail.com>
2014-06-02 16:32:03 -0600

    Added another way to min/max

--- python_files/get_min_max_num_input.py
+++ python_files/get_min_max_num_input.py
@@ -1,24 +1,47 @@
 # Getting min and max numbers from user input
 
+##minimum = None
+##maximum = None
+##
+##while True:
+##    num = input('Enter a number: ')
+##    if num == 'done': 
+##        break
+##
+##    try:
+##        num_check = float(num)
+##    except:
+##        print ('Invalid input')
+##        continue                            
+##
+##    if minimum is None or num_check < minimum:
+##        minimum = num_check
+##
+##    if maximum is None or num_check > maximum:
+##        maximum = num_check
+##
+##print ('Maximum is', maximum)
+##print ('Minimum is', minimum)
+
+# Same as above but with built-in functions and list
+
+mylist = []
+
 minimum = None
 maximum = None
 
 while True:
-    num = input('Enter a number: ')
-    if num == 'done': 
-        break
-
+    num = input('Enter a number:')
+    if num == 'done': break
     try:
         num_check = float(num)
+        mylist.append(num_check)
     except:
-        print ('Invalid input')
-        continue                            
-
-    if minimum is None or num_check < minimum:
-        minimum = num_check
-
-    if maximum is None or num_check > maximum:
-        maximum = num_check
-
+        print('Invalid input')
+        continue
+    maximum = max(mylist)
+    minimum = min(mylist)
 print ('Maximum is', maximum)
 print ('Minimum is', minimum)
+print ('From list you gave:',mylist)
+
