cf46a1dd541e9f4e9bba7ab6ec2d987c1db9f9f3
pTricKg <ptrickg1976@gmail.com>
2014-06-02 16:32:59 -0600

    Uncommented out first loop

--- python_files/get_min_max_num_input.py
+++ python_files/get_min_max_num_input.py
@@ -1,27 +1,27 @@
 # Getting min and max numbers from user input
 
-##minimum = None
-##maximum = None
-##
-##while True:
-##    num = input('Enter a number: ')
-##    if num == 'done': 
-##        break
-##
-##    try:
-##        num_check = float(num)
-##    except:
-##        print ('Invalid input')
-##        continue                            
-##
-##    if minimum is None or num_check < minimum:
-##        minimum = num_check
-##
-##    if maximum is None or num_check > maximum:
-##        maximum = num_check
-##
-##print ('Maximum is', maximum)
-##print ('Minimum is', minimum)
+minimum = None
+maximum = None
+
+while True:
+    num = input('Enter a number: ')
+    if num == 'done': 
+        break
+
+    try:
+        num_check = float(num)
+    except:
+        print ('Invalid input')
+        continue                            
+
+    if minimum is None or num_check < minimum:
+        minimum = num_check
+
+    if maximum is None or num_check > maximum:
+        maximum = num_check
+
+print ('Maximum is', maximum)
+print ('Minimum is', minimum)
 
 # Same as above but with built-in functions and list
 
