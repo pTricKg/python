d7706b4d7ebae00b102dfc3d4bd173428cf97977
pTricKg <ptrickg1976@gmail.com>
2014-06-05 21:30:46 -0600

    Add comments and some change

--- python_files/read_email_part_three.py
+++ python_files/read_email_part_three.py
@@ -5,22 +5,27 @@ mydict = {}
 count = 0
 
 for line in handle:
+    # pull lines that start with From with space after
     if line.startswith("From "): #added  space to be sure not to include From:
+        #split that line
         splt = line.split()
 
+        # pull out index 1 of split string
         print("This is email address:",splt[1]) # this is what we're looking for
+        # set that to variable
         eml = splt[1]
         print(eml)
+        # increment counter
         count = count + 1
         print("this is count:",count)
         #mydict = mydict[splt[1]]
-        mydict[splt[1]] = 1
+        mydict[eml] = 1
         print(mydict)
         
-        for eml in mydict:
+        if eml == mydict:
             mydict[splt[1]] = mydict[splt[1]] + 1
         print(mydict)
     else:
         continue
-print(count,mydict)
+print("Final count:",count,"Final dictionary:",mydict)
 
