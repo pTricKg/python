4e834c8181c007052234dc1a661bca9e982cb5dd
pTricKg <ptrickg1976@gmail.com>
2014-06-09 21:33:23 -0600

    Add test for appendsums function

---
+++ python_files/testing_appendsums.py
@@ -0,0 +1,16 @@
+##def appendsums(lst):
+##    """
+##    Repeatedly append the sum of the current last three elements of lst to lst.
+##    """
+##
+##    for n in lst:
+lst = [0,1,2]
+
+lst_len = len(lst)
+val1 = n[lst_len]
+val2 = n[lst_len - 1]
+val3 = n[lst_len - 2]
+
+print(val1,val2,val3)
+
+        
