33400a064e0f5af0fc090d9d544904157280e9ff
pTricKg <ptrickg1976@gmail.com>
2014-06-10 00:28:09 -0600

    Fixed up, works sort of

--- python_files/with_docstring/not_done_bankaccount.py
+++ python_files/with_docstring/not_done_bankaccount.py
@@ -1,24 +1,31 @@
 class BankAccount:
     def __init__(self, initial_balance):
         """Creates an account with the given balance."""
-        my_account = initial_balance
+        self.initial_balance = initial_balance
         
     def deposit(self, amount):
         """Deposits the amount into the account."""
-        my_account = my_account + amount
-        return my_account
+        self.amount = self.initial_balance + amount
+        return amount
     def withdraw(self, amount):
         """
         Withdraws the amount from the account.  Each withdrawal resulting in a
         negative balance also deducts a penalty fee of 5 dollars from the balance.
         """
-        my_account = my_account - amount
-        return my_account
+        self.amount = self.initial_balance - amount
+        return self.amount
     def get_balance(self):
         """Returns the current balance in the account."""
-        return my_account
+        return self.initial_balance
     def get_fees(self):
         """Returns the total fees ever deducted from the account."""
-        if my_account < 0:
-            fee = my_account - 5
-        return fee    
+        if self.initial_balance < 0:
+            fee = self.initial_balance - 5
+        return fee
+
+
+# To Test:
+my_account = BankAccount(10)
+#my_account.withdraw(15)
+my_account.deposit(20)
+print (my_account.get_balance())
