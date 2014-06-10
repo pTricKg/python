cba3feccc0719ed0bd9a1a2c25d2b4f9a9138a20
pTricKg <ptrickg1976@gmail.com>
2014-06-10 01:02:49 -0600

    Added descriptive comment

--- python_files/with_docstring/not_done_bankaccount.py
+++ python_files/with_docstring/not_done_bankaccount.py
@@ -1,25 +1,33 @@
 class BankAccount:
+
+    """ A Simple Bank Accounting Program """
+    
     def __init__(self, initial_balance):
         """Creates an account with the given balance."""
         self.initial_balance = initial_balance
+##        super(BankAccount, self).__init__()
         
     def deposit(self, amount):
         """Deposits the amount into the account."""
         self.amount = self.initial_balance + amount
-        return self.amount 
+        return self.amount
+    
     def withdraw(self, amount):
         """
         Withdraws the amount from the account.  Each withdrawal resulting in a
         negative balance also deducts a penalty fee of 5 dollars from the balance.
         """
         self.amount = self.initial_balance - amount
+        
         if self.initial_balance - self.amount < 0:
             fee = self.initial_balance - 5
             return fee
         return self.amount
+    
     def get_balance(self):
         """Returns the current balance in the account."""
         return self.initial_balance
+    
     def get_fees(self):
         """Returns the total fees ever deducted from the account."""
         
@@ -31,3 +39,4 @@ my_account = BankAccount(10)
 my_account.withdraw(15)
 my_account.deposit(5)
 print (my_account.get_balance())
+
