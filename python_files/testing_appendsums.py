f9cab30047040e5bca1a77839deec50124a64ec5
pTricKg <ptrickg1976@gmail.com>
2014-06-09 21:39:01 -0600

    step one

--- python_files/testing_appendsums.py
+++ python_files/testing_appendsums.py
@@ -5,11 +5,12 @@
 ##
 ##    for n in lst:
 lst = [0,1,2]
+lst_len = len(lst) - 1
+print(lst_len)
 
-lst_len = len(lst)
-val1 = n[lst_len]
-val2 = n[lst_len - 1]
-val3 = n[lst_len - 2]
+val1 = lst[lst_len]
+val2 = lst[lst_len - 1]
+val3 = lst[lst_len - 2]
 
 print(val1,val2,val3)
 
