e07d3b480d427335667df91497d01ceeb00e351d
pTricKg <ptrickg1976@gmail.com>
2014-06-09 21:48:15 -0600

    Not work with loop

--- python_files/testing_appendsums.py
+++ python_files/testing_appendsums.py
@@ -1,17 +1,20 @@
-##def appendsums(lst):
-##    """
-##    Repeatedly append the sum of the current last three elements of lst to lst.
-##    """
+def appendsums(lst):
+    """
+    Repeatedly append the sum of the current last three elements of lst to lst.
+    """
+
+    # first get length of list
+    lst_len = len(lst) - 1 # must subtract one to actual indexing from 0
+    for n in lst:
+
+        val1 = n[lst_len]
+        val2 = n[lst_len - 1]
+        val3 = n[lst_len - 2]
+
+        final = val1 + val2 + val3
+
+        print(val1,val2,val3,"and final sum:",final)
+
+## my_list = [0,2,3]
 ##
-##    for n in lst:
-lst = [0,1,2]
-lst_len = len(lst) - 1
-print(lst_len)
-
-val1 = lst[lst_len]
-val2 = lst[lst_len - 1]
-val3 = lst[lst_len - 2]
-
-print(val1,val2,val3)
-
-        
+## appendsums(my_list)
