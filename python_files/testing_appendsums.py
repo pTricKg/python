1610da9b189a752c0d69e43267a92c98e43e6beb
pTricKg <ptrickg1976@gmail.com>
2014-06-09 22:32:49 -0600

    trying for loop

--- python_files/testing_appendsums.py
+++ python_files/testing_appendsums.py
@@ -4,17 +4,27 @@ def appendsums(lst):
     """
 
     # first get length of list
-    lst_len = len(lst) - 1 # must subtract one to actual indexing from 0
-    for n in lst:
+##    lst_len = len(lst) - 1 # must subtract one to actual indexing from 0
+##    for n in lst:
 
-        val1 = n[lst_len]
-        val2 = n[lst_len - 1]
-        val3 = n[lst_len - 2]
+##    val1 = lst[lst_len]
+##    val2 = lst[lst_len - 1]
+##    val3 = lst[lst_len - 2]
+##
+##    final = val1 + val2 + val3
+##
+##    print(val1,val2,val3,"and final sum:",final)
+    
+    
+    for vals in lst:
 
-        final = val1 + val2 + val3
-
-        print(val1,val2,val3,"and final sum:",final)
+        print("From list",lst[vals])
+        lst.append(vals)
+        print("Append",lst)
+        
+        
 
 ## my_list = [0,2,3]
 ##
 ## appendsums(my_list)
+##appendsums([0,1,2])
