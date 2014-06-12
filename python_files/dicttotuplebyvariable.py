a6a9b1475ea16c5bab770e72806b0d176094da66
pTricKg <ptrickg1976@gmail.com>
2014-06-12 11:14:24 -0600

    Small comment edit

--- python_files/dicttotuplebyvariable.py
+++ python_files/dicttotuplebyvariable.py
@@ -3,7 +3,7 @@ d = {'a':20, 'b':5, 'c':10} # created dict
 tmplst = [] # empty list to be filled with tuples
 
 for k, v in d.items(): # go thru key:value pairs
-    tmplst.append( (v, k) ) # append them to list with value:key
+    tmplst.append( (v, k) ) # append tuples to list with value:key
 
 print(tmplst)
 
