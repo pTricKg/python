6ef73e01a2da20de94830a7d5201c808917c0e3d
pTricKg <ptrickg1976@gmail.com>
2014-06-12 13:44:21 -0600

    Edit code and remove some prints

--- python_files/findtimefromtxt.py
+++ python_files/findtimefromtxt.py
@@ -8,33 +8,29 @@ lst = list()
 
 for lines in handle:
     if lines.startswith("From "):
-        print(lines)
+        
         # find space before time starts
         fnd = lines.find(" ",lines.find(" ",lines.find\
                                         (" ",lines.find\
                                          (" ",lines.find\
                                           (" ",lines.find(" ")\
                                            + 1)+ 1)+ 1)+ 1)+ 1)
-        print(fnd)
         time = lines[fnd + 1:fnd + 3]
         lst.append(time)
-        print(lst)
-        print(time)
+        
         counts = dict()
         for count in lst:
             counts[count] = counts.get(count, 0) + 1
 
-        print(counts)
+tmplist = list()
 
-        tmplist = list()
+for key, val in counts.items():
+    tmplist.append( (key, val) ) # append tuples
 
-        for key, val in counts.items():
-            tmplist.append( (key, val) ) # append tuples
+tmplist.sort()
 
-        tmplist.sort()
-
-        for key, val in tmplist:
-            print(key, val)
-            
+for key, val in tmplist:
+    print(key, val)
+        
         
     
