1e054fec8fd05c8250fe4d88b22a5e22c6a75b55
pTricKg <ptrickg1976@gmail.com>
2014-06-12 11:24:58 -0600

    get 10 most common words from txt

---
+++ python_files/gettentopwordsintext.py
@@ -0,0 +1,18 @@
+txttoread = open('romeo.txt')
+
+counts = dict()
+
+for line in txttoread:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+lst = list()
+
+for key, val in counts.items():
+    lst.append( (val, key) )
+
+lst.sort(reverse=True)
+
+for val, key in lst[:10]:
+    print(key, val)
