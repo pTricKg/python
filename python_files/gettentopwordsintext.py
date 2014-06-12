a25f4b8b8243ed37e7ca5b541f5bcca65b9201d6
pTricKg <ptrickg1976@gmail.com>
2014-06-12 11:31:34 -0600

    Added comments

--- python_files/gettentopwordsintext.py
+++ python_files/gettentopwordsintext.py
@@ -6,13 +6,15 @@ for line in txttoread:
     words = line.split()
     for word in words:
         counts[word] = counts.get(word, 0) + 1
+        # above looks if word in dict, if it is add word to dict
+        # if it isn't, default to 0
 
-lst = list()
+lst = list() # set temp list for sorting tuples
 
 for key, val in counts.items():
-    lst.append( (val, key) )
+    lst.append( (val, key) ) # append tuples into temp list
 
-lst.sort(reverse=True)
+lst.sort(reverse=True) # reverse sort by val
 
-for val, key in lst[:10]:
+for val, key in lst[:10]: # go through val up to 10
     print(key, val)
