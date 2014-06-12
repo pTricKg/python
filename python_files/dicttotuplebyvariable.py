49b61bb334006b2ffa8d57d275d265e031dd6e1b
pTricKg <ptrickg1976@gmail.com>
2014-06-12 11:12:53 -0600

    Added new

---
+++ python_files/dicttotuplebyvariable.py
@@ -0,0 +1,12 @@
+d = {'a':20, 'b':5, 'c':10} # created dict
+
+tmplst = [] # empty list to be filled with tuples
+
+for k, v in d.items(): # go thru key:value pairs
+    tmplst.append( (v, k) ) # append them to list with value:key
+
+print(tmplst)
+
+tmplst.sort(reverse=True) # sort in reverse 
+
+print(tmplst)
