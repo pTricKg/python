7f37acde2eadf5bb3751601780d3fb4e21be0c2a
pTricKg <ptrickg1976@gmail.com>
2014-06-23 21:00:48 -0600

    Add regex descript for added for loop

--- python_files/test_regex2.py
+++ python_files/test_regex2.py
@@ -9,3 +9,13 @@ for line in hand:
 # ^ equals begin of line
 # * any number of times
 # . match any character
+# \S match any non-whitespace character
+# +  one or more times
+
+import re
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^X-\S+',line): # if line starts with From:
+        print(line)
