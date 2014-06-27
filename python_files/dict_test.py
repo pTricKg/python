1d02f6448b7dc52aa518be5d23d4dab0d3faa358
pTricKg <ptrickg1976@gmail.com>
2014-06-27 15:05:54 -0600

    more concise dict loop

--- python_files/dict_test.py
+++ python_files/dict_test.py
@@ -5,34 +5,49 @@ rd = opn.read()
 splt = rd.split()
 d = {}
 
-for i in splt:
-    if i not in d:
-        d[i] = 1
-    else:
-        d[i] = d[i] + 1
+##for i in splt:
+##    if i not in d:
+##        d[i] = 1
+##    else:
+##        d[i] = d[i] + 1
+##
+##print(d)
 
+## Better way to get same done as above
+for i in splt:
+    d[i] = d.get(i,0) + 1
 print(d)
 
-val = d.values()
 
-print(val)
-vlist = []
-dtwo = {}
+dval = d.values()
+total = 0
 
-for v in val:
-    vlist.append(v)
-    for i in vlist:
-        if i >= 5:
-            if i not in dtwo:
-                dtwo[i] = dtwo[i] + 1
-            else:
-                dtwo[i] = dtwo[i] + 1
-print(dtwo)
+for i in dval:
+    total = total + i
+print("Total:", total)
+    
+
+
+##val = d.values()
+##
+##print(val)
+##vlist = []
+##dtwo = {}
+##
+##for v in val:
+##    vlist.append(v)
+##    for i in vlist:
+##        if i >= 5:
+##            if i not in dtwo:
+##                dtwo[i] = dtwo[i] + 1
+##            else:
+##                dtwo[i] = dtwo[i] + 1
+##print(dtwo)
 
 
 """ Using dictionary to find and count freq """
 
-##opn = open('mbox-short.txt')
+##opn = open('nineteen_eighty_four.txt')
 ##rd = opn.read()
 ##splt = rd.split()
 ##d = {}
