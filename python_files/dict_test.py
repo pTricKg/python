e18dfab935220dc5ca7435afc2f6dedf8486746b
pTricKg <ptrickg1976@gmail.com>
2014-06-27 15:30:53 -0600

    uncomment

--- python_files/dict_test.py
+++ python_files/dict_test.py
@@ -1,42 +1,42 @@
 """ Using dictionary to count word frequency in text """
 
-##opn = open('nineteen_eighty_four.txt')
-##rd = opn.read()
-##splt = rd.split()
-##d = {}
-##
-####for i in splt:
-####    if i not in d:
-####        d[i] = 1
-####    else:
-####        d[i] = d[i] + 1
-####
-####print(d)
-##
-#### Better way to get same done as above
-##for i in splt:
-##    d[i] = d.get(i,0) + 1
-##print(d)
-##
-##
-##dval = d.values()
-##total = 0
-##
-##for i in dval:
-##    total = total + i
-##print("Total:", total)
-##    
-##
-##
-##val = d.values()
-##dtwo = {}
-##
-##print(val)
-##
-##for k,v in d.items():
-##    if v >= 5:
-##       dtwo[k] = v
-##print("Second dictionary:",dtwo)
+opn = open('nineteen_eighty_four.txt')
+rd = opn.read()
+splt = rd.split()
+d = {}
+
+for i in splt:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] = d[i] + 1
+
+print(d)
+
+## Better way to get same done as above
+for i in splt:
+    d[i] = d.get(i,0) + 1
+print(d)
+
+
+dval = d.values()
+total = 0
+
+for i in dval:
+    total = total + i
+print("Total:", total)
+    
+
+
+val = d.values()
+dtwo = {}
+
+print(val)
+
+for k,v in d.items():
+    if v >= 5:
+       dtwo[k] = v
+print("Second dictionary:",dtwo)
 
 
 
