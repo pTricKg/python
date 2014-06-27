2a62ffb55ba2542dca19aafc2d3386262f3ff263
pTricKg <ptrickg1976@gmail.com>
2014-06-27 15:30:13 -0600

    some more fun

--- python_files/dict_test.py
+++ python_files/dict_test.py
@@ -1,57 +1,59 @@
 """ Using dictionary to count word frequency in text """
 
-opn = open('nineteen_eighty_four.txt')
-rd = opn.read()
-splt = rd.split()
-d = {}
-
-##for i in splt:
-##    if i not in d:
-##        d[i] = 1
-##    else:
-##        d[i] = d[i] + 1
-##
-##print(d)
-
-## Better way to get same done as above
-for i in splt:
-    d[i] = d.get(i,0) + 1
-print(d)
-
-
-dval = d.values()
-total = 0
-
-for i in dval:
-    total = total + i
-print("Total:", total)
-    
-
-
-val = d.values()
-dtwo = {}
-
-print(val)
-
-for k,v in d.items():
-    if v >= 5:
-       dtwo[k] = v
-print("Second dictionary:",dtwo)
-
-
-
-""" Using dictionary to find and count freq """
-
 ##opn = open('nineteen_eighty_four.txt')
 ##rd = opn.read()
 ##splt = rd.split()
 ##d = {}
 ##
-##for i in splt:
-##    if i.find('@') > 0:
-##        if i not in d:
-##            d[i] = 1
-##        else:
-##            d[i] = d[i] + 1
+####for i in splt:
+####    if i not in d:
+####        d[i] = 1
+####    else:
+####        d[i] = d[i] + 1
+####
+####print(d)
 ##
+#### Better way to get same done as above
+##for i in splt:
+##    d[i] = d.get(i,0) + 1
 ##print(d)
+##
+##
+##dval = d.values()
+##total = 0
+##
+##for i in dval:
+##    total = total + i
+##print("Total:", total)
+##    
+##
+##
+##val = d.values()
+##dtwo = {}
+##
+##print(val)
+##
+##for k,v in d.items():
+##    if v >= 5:
+##       dtwo[k] = v
+##print("Second dictionary:",dtwo)
+
+
+
+""" Using dictionary to find and count freq """
+
+opn = open('nineteen_eighty_four.txt')
+rd = opn.read().lower()
+splt = rd.split()
+d = {}
+
+for i in splt:
+    if 'speak' in i or 'double' in i or 'think' in i or 'party' in i:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] = d[i] + 1
+    else:
+        continue
+
+print(d)
