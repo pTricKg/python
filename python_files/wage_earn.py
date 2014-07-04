f7531b35937663af05b5de777383023ab91c21f9
pTricKg <ptrickg1976@gmail.com>
2014-07-03 22:39:58 -0600

    small edit

--- python_files/wage_earn.py
+++ python_files/wage_earn.py
@@ -8,12 +8,14 @@ inp2 = float(input("How many hours are you working: "))
 
 if inp2 > 40:
     wkly = (((inp / 2) + inp) * (inp2 - 40) + (inp * inp2))
+    print("This is your earnings for hours given\
+ with overtime:",round(wkly,2))
 else:
-   wkly = inp * inp2 
-print("This is your earnings for hours given:",round(wkly,2))
-mnthly = (wkly * 52) / 12
-print("This is your monthly earnings:",round(mnthly,2))
-annum = wkly * 52
-print("This is your annual earnings:",round(annum,2))
+    wkly = inp * inp2 
+    print("This is your earnings for hours given:",round(wkly,2))
+    mnthly = (wkly * 52) / 12
+    print("This is your monthly earnings:",round(mnthly,2))
+    annum = wkly * 52
+    print("This is your annual earnings:",round(annum,2))
 
 #def wage()
