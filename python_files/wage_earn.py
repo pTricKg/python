20fafed03fe2d7e168b56c7dd7094ac4ac1aa99f
pTricKg <ptrickg1976@gmail.com>
2014-07-03 22:44:42 -0600

    added comment

--- python_files/wage_earn.py
+++ python_files/wage_earn.py
@@ -4,7 +4,8 @@
 inp = float(input("What is your hourly rate: "))
 inp2 = float(input("How many hours are you working: "))
 
-
+print("\nIf you've worked overtime, you'll only be shown weeks pay.  All pay is\
+ calculated in pre-tax dollars\n")
 
 if inp2 > 40:
     wkly = (((inp / 2) + inp) * (inp2 - 40) + (inp * inp2))
