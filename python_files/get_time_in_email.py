137928fccb6af1baccce88947f8a5571c7c37c50
pTricKg <ptrickg1976@gmail.com>
2014-07-16 22:04:56 -0600

    add one

---
+++ python_files/get_time_in_email.py
@@ -0,0 +1,38 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name, 'r')
+mydict = {}
+count = 0
+
+for line in handle:
+    # pull lines that start with From with space after
+    if line.startswith("From "): #added  space to be sure not to include From:
+        #split that line
+        splt = line.split()
+
+        time = splt[5]
+        timesplt = time.split(':')
+        
+        tm = timesplt[0]
+        print(tm)
+        
+        count = count + 1
+        
+##        if tm not in mydict:
+##            mydict[tm] = 1
+##            
+##        # then if it is, increment that by one
+##        else:
+##            mydict[tm] = mydict[tm] + 1
+##            print(mydict)
+
+        # or same as if else above:
+        mydict[tm] = mydict.get(tm,0) + 1
+             
+    else:
+        continue
+print(mydict, count)
+
+
+    
+
