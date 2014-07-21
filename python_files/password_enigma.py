e58a442eda840fedc6f07fab4d808d78d53a74d5
pTricKg <ptrickg1976@gmail.com>
2014-07-20 19:15:28 -1200

    formatting

--- python_files/password_enigma.py
+++ python_files/password_enigma.py
@@ -1,16 +1,19 @@
 count=3
 x=0
 password = input('Enter password :')
+
 for i in password:
     x = ord(i) * count + x
     count += 1
     print(count)
+
 ##    print(x)
 ##    z = ""
 ##    for j in str(x):
 ##        z = chr(j) + z
 ##        print (z)
 ##print ("z is : " + z)
+
 print (password)
 print ('The countis: ' + str(count))
 print ('The sum is: ' +str(x))
@@ -22,6 +25,7 @@ print ('The sum is: ' +str(x))
 count=5
 x=0
 password = input('Enter password :')
+
 for i in password:
     #converting to ASCII
     x = ord(i) * count + x
