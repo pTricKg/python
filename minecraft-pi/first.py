73715093a41208b74a57ec64916599e065e4c691
pTricKg <ptrickg@hotmail.com>
2014-08-07 23:27:46 +0000

    catch up

---
+++ minecraft-pi/first.py
@@ -0,0 +1,6 @@
+count = 0
+
+while count < 5:
+	print("you momma don't wear shoes")
+	count += 1
+
---
+++ minecraft-pi/teleportPrecise.py
@@ -0,0 +1,14 @@
+# import minecraft api necessities
+
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+
+''' Do not use coordinates > 127 on x and z'''
+
+# coordinates to teleport to
+# home >
+x = 76.4
+y = 7.1
+z = 106.3
+
+mc.player.setPos(x,y,z)
