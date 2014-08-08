f949e5c0f4694908fef7e8ac6c9af676dddf5927
pTricKg <ptrickg1976@gmail.com>
2014-08-07 19:20:21 -0600

    fix up comment stuff doo

--- minecraft-pi/teleport.py
+++ minecraft-pi/teleport.py
@@ -3,8 +3,9 @@
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
 ''' Do not use coordinates > 127 on x and z'''
+
 # coordinates to teleport to
-# home >
+
 x = 40
 y = 7
 z = 83
@@ -12,4 +13,3 @@ z = 83
 # move player
 mc.player.setTilePos(x,y,z)   
 
-          
