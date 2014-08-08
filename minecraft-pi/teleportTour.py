b727698401990160fc15773e9c5be6ef8cdbb647
pTricKg <ptrickg@hotmail.com>
2014-08-08 00:57:56 +0000

    some more fun

---
+++ minecraft-pi/teleportTour.py
@@ -0,0 +1,32 @@
+# import minecraft api necessities
+
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+import time
+
+''' Do not use coordinates > 127 on x and z'''
+
+x = 76
+y = 7
+z = 106
+
+time.sleep(5)
+
+x = 40
+y = 7
+z = 83
+
+time.sleep(5)
+
+x = 10
+y = 11
+z = 50
+
+time.sleep(5)
+
+x = 76
+y = 7
+z = 106
+
+# move player
+mc.player.setTilePos(x,y,z)
