a3cc0efaa44935bb87e910cb461bf4d926b135fc
ptrickg <ptrickg1976@gmail.com>
2015-07-16 20:33:04 -0600

    fizz_buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,21 @@
+print("Going to print fizz if number divisible by 3 and buzz if divisible 5.")
+
+num = 0
+
+count_fizz = 0
+count_buzz = 0
+
+while num < 100:
+    num += 1
+    ## check if divisible by 3
+    if num % 3 == 0:
+        count_fizz += 1
+        print(num, '= fizz')
+    ## check if divisible by 5
+    elif num % 5 == 0:
+        count_buzz += 1
+        print(num, '= buzz')
+    else:
+        print(num)
+print("Fuzz count: ", count_fizz)
+print("Buzz count: ", count_buzz)
