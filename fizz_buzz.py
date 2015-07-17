d4ceaeb18cc84cb1bfe91418f4b370f1a94b5bce
ptrickg <ptrickg1976@gmail.com>
2015-07-16 20:41:54 -0600

    some formatting

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -7,15 +7,20 @@ count_buzz = 0
 
 while num < 100:
     num += 1
+
     ## check if divisible by 3
     if num % 3 == 0:
         count_fizz += 1
         print(num, '= fizz')
+
     ## check if divisible by 5
     elif num % 5 == 0:
         count_buzz += 1
         print(num, '= buzz')
+
+    ## or just print number
     else:
         print(num)
+        
 print("Fuzz count: ", count_fizz)
 print("Buzz count: ", count_buzz)
