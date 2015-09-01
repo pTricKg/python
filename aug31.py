9969fb85eb5b38770a901710067f6e11b009a3e2
ptrickg <ptrickg1976@gmail.com>
2015-09-01 03:00:37 -0600

    play

---
+++ aug31.py
@@ -0,0 +1,38 @@
+word = input("Give me a word and i'll give you x's for all vowels >")
+##for i in word:
+##    if i == "e":
+##        i = "y"
+##        word = word[i]
+##        print(i)
+##        
+##    print(word)
+
+count = 0
+while count < len(word):
+    
+    for i in word:
+        if count == 1:
+            n = i.upper()
+            print(i)
+            word = word.replace(i, n)
+        elif count == len(word) - 2:
+            n = i.upper()
+            print(i)
+            word = word.replace(i, n)
+        elif i == "a":
+            word = word.replace(i,"x")
+        elif i == "e":
+            word = word.replace(i,"x")
+        elif i == "i":
+            word = word.replace(i,"x")
+        elif i == "o":
+            word = word.replace(i,"x")
+        elif i == "u":
+            word = word.replace(i,"x")
+    
+        count += 1
+        print(count)    
+            
+print(word)
+
+## or "i" or "o" or "a" or "u"
