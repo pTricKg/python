2939058017a19d7022b397707a4591398b44ac83
ptrickg <ptrickg1976@gmail.com>
2015-11-03 00:17:22 -0700

    some del and fin

--- JourneymanActivities.py
+++ JourneymanActivities.py
@@ -49,7 +49,11 @@ def journeyman2(final_num):
 '''1.3) Write a python script which connects to the included server 
 on port 50001 and returns the message it receives.'''
 def journeyman3():
-    return
+    s = socket.socket(socket.AF_INET , socket.SOCK_STREAM) #est connection
+    s.connect(('127.0.0.10' , 50007)
+    received_string = s.recv(1024)
+    s.close()
+    return received_string
 
 
 '''1.4) Create a class called person, with height, weight, hair color, 
